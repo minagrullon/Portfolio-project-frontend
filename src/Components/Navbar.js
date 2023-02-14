1d13e7e21d5bfe6dbece7c1c623028bc7980837f
minagrullon <minagrullon@pursuit.org>
2023-02-14 12:28:29 -0500

    home page touched

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -18,7 +18,10 @@ export default function Navbar() {
           Doggo Central
         </Link>
       </h1>
-      <Link className="nav_button" to="/items/new">
+      <Link
+        className=" text-yellow-400 px-5  hover:text-white border border-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-sm  text-center mr-2  dark:border-yellow-300 dark:text-yellow-300 dark:hover:text-white dark:hover:bg-yellow-400 dark:focus:ring-yellow-900"
+        to="/items/new"
+      >
         <button className="nav_new">Add an item</button>
       </Link>
       <Link to="/about">
