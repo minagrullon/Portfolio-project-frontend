4641e5c38bc6f88be53e45cd537de8bd2710079e
minagrullon <minagrullon@pursuit.org>
2023-02-14 21:26:21 -0500

    about pg progress

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,13 +1,37 @@
 import React from "react";
-import blanco from "../blanco-code.jpg";
+import blanco from "../blanco-code.jpeg";
+import poki from "../poki-nom.jpeg";
 
 export default function About() {
   return (
-    <div className="">
-      <h1>About</h1>
-      <p>What inspired the project?</p>
-
-      <img src={blanco} alt="the boss himself" />
+    <div className="flex justify-evenly bg-sky-100 h-screen">
+      <div className="pt-40 ">
+        <img
+          className="p-2 bg-white border rounded max-w-sm"
+          src={poki}
+          alt="lil-baby"
+        />
+      </div>
+      <div className="p-14 border-x-black w-6/12">
+        <h1 className="font-medium leading-tight text-4xl mt-0 mb-2 text-blue-400 p-10">
+          About
+        </h1>
+        <p className="text-lg font-medium mb-5">What inspired the project?</p>
+        <p>
+          Going to pet stores and seeing all the options of products can be
+          overwhelming. What is actually worth the investment? I would want to
+          give my dogs the very best. It would be helpful to have suggestions
+          rather than search thru a whole website's catalog of options. Thus
+          Doggo Central was born!
+        </p>
+      </div>
+      <div className="pt-10">
+        <img
+          className="p-2 bg-white border rounded max-w-sm"
+          src={blanco}
+          alt="the boss himself"
+        />
+      </div>
     </div>
   );
 }
--- src/Pages/Index.js
+++ src/Pages/Index.js
@@ -2,12 +2,11 @@ import React from "react";
 import { useState } from "react";
 import Items from "../Components/Items";
 import Category from "../Components/Category";
-import "./Index.css";
 
 export default function Index() {
   const [category, setCategory] = useState("");
   return (
-    <div>
+    <div className="bg-orange-50">
       <Category setCategory={setCategory} />
       <h2 className="text-2xl font-bold text-blue-500 mb-8 ml-8">
         Recommended Products
